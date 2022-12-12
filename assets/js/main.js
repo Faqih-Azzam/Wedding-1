d66db277ac8b898be3c9cb11f8173225dba100ff
Faqih Azzam <faqih.farhan.778@gmail.com>
2022-12-12 19:53:41 +0700

    Wedding-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,29 @@ function myFunction() {
     moreText.style.display = "block";
   }
 }
+
+let countDownDate = new Date("Dec 13, 2022 00:00:00").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  let timeDays = 60 * 60 * 1000 * 24;
+  let timeHours = 60 * 60 * 1000;
+
+  days = Math.floor((distance / timeDays) * 1);
+  years = Math.floor(days / 365);
+  if (years > 1) {
+    days = days - years * 365;
+  }
+  hours = Math.floor(((distance % timeDays) / timeHours) * 1);
+
+  document.getElementById("demo").innerHTML =
+    years + "Tahun " + days + "Hari " + hours + "Jam ";
+
+  // If the count down is over, write some text
+  // if (distance < 0) {
+  //   clearInterval(x);
+  //   document.getElementById("demo").innerHTML = "EXPIRED";
+  // }
+}, 1000);
